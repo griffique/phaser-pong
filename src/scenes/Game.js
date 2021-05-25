2aa85356ef226474414d6945683fc397f8b4cb54
Quinn <quinneringriffin@gmail.com>
2021-05-25 15:18:22 -0400

    added paddle movement

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,12 +8,25 @@ export default class Game extends Phaser.Scene {
         ball.body.setVelocity(-200, 0);
         ball.body.setCollideWorldBounds(true, 1, 1);
         ball.body.setBounce(1, 1)
-        const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
-        this.physics.add.existing(paddleLeft, true);
-        this.physics.add.collider(paddleLeft, ball);
+        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1);
+        this.physics.add.existing(this.paddleLeft, true);
+        this.physics.add.collider(this.paddleLeft, ball);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
 
 
+
+    }
+    update() {
+        const body = this.paddleLeft.body;
+        if (this.cursors.up.isDown) {
+            this.paddleLeft.y -= 10
+            body.updateFromGameObject()
+        }
+        else if (this.cursors.down.isDown) {
+            this.paddleLeft.y += 10
+            body.updateFromGameObject()
+        }
 
     }
 }
